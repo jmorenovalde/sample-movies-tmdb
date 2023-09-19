d51ed4095f80892285ba1c5d37934cf142be85b5
Juan Antonio Moreno Valderrama <jamv999@gmail.com>
2023-09-19 20:17:23 +0200

    add changeDetection onPush

--- src/app/shared/components/movies/movie-card/movie-card.component.ts
+++ src/app/shared/components/movies/movie-card/movie-card.component.ts
@@ -1,10 +1,11 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 import { Movie } from 'src/app/shared/models/movie.model';
 
 @Component({
   selector: 'app-movie-card',
   templateUrl: './movie-card.component.html',
-  styleUrls: ['./movie-card.component.scss']
+  styleUrls: ['./movie-card.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MovieCardComponent {
   @Input({required: true}) movie!: Movie;
